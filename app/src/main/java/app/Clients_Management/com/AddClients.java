e7c4b6ba3141215e313b6d8f387d47208b58ca52
AyaOsamaAhmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2018-12-23 17:00:45 +0200

    send database

--- app/src/main/java/app/Clients_Management/com/AddClients.java
+++ app/src/main/java/app/Clients_Management/com/AddClients.java
@@ -2,15 +2,25 @@ package app.Clients_Management.com;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by egypt2 on 11-Dec-18.
@@ -22,6 +32,7 @@ public class AddClients extends Activity {
     TextView        total ;
     Button          button_save ;
     String          ls_id ,ls_name , ls_phone , ls_card , ls_cash ="0.0" , ls_buy="0.0" , ls_total="0.0" , ls_date ;
+    Double          ld_buy=0.0 , ld_cash=0.0 , ld_total=0.0 ;
     String          databasename;
     DataClients     dataClients;
 
@@ -31,7 +42,7 @@ public class AddClients extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addclients);
         //-------Database Firebase
-        databasename = "Test";                                                      // name clients
+        databasename = "Users";                                                      // name clients
         databaseclients = FirebaseDatabase.getInstance().getReference(databasename);
         //------------------- Declear
         name = (EditText)findViewById(R.id.name);
@@ -43,9 +54,10 @@ public class AddClients extends Activity {
         date = (EditText)findViewById(R.id.date);
         button_save =(Button)findViewById(R.id.button_save);
 
-        //--------- calc Total
+        //--------- Set Data
         setData();
-        buy.addTextChangedListener(new TextWatcher() {
+        //------ Calc Total
+      buy.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override
@@ -67,11 +79,24 @@ public class AddClients extends Activity {
                 CalcTotal();
             }
         });
-        //-------- Set Data
-        setData();
-        dataClients  = new DataClients(ls_id ,ls_name,ls_phone,ls_card,ls_cash,ls_buy,ls_date);
-        //--------- Button Save
 
+
+        //-------- Set Data
+        CalcTotal();
+
+        //--------- Button Save
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setData();
+                String  id = databaseclients.push().getKey();
+                ls_id = id ;
+                dataClients  = new DataClients(ls_id ,ls_name,ls_phone,ls_card,ls_cash,ls_buy,ls_date);
+
+                databaseclients.child(id).setValue(dataClients);
+                Toast.makeText(AddClients.this, "Saved Data Sucsses", Toast.LENGTH_SHORT).show();
+            }
+        });
         //---------
 
     }
@@ -82,15 +107,28 @@ public class AddClients extends Activity {
         ls_card  = card.getText().toString();
         ls_cash = cash.getText().toString();
         ls_buy  = buy.getText().toString();
-        ls_date = date.getText().toString();
+        ls_date =date.getText().toString();
+        //------
+      /*  Date calendar = Calendar.getInstance().getTime();
+        Integer day = calendar.getDate();
+        Integer month = calendar.getMonth();
+        Integer year = calendar.getYear();
+
+        //united destriputer - -115-
+        date.setText(year+"/"+month+"/"+day);
+        date.setText(year.toString());*/
 
     }
 
     private void  CalcTotal (){
-        Integer total ;
+        //-------
+        if (ls_buy.isEmpty()) ls_buy="0.0";
+        if(ls_cash.isEmpty()) ls_cash="0.0";
         //--------
-        total = Integer.parseInt(ls_buy) - Integer.parseInt(ls_cash) ;
-        ls_total = total.toString() ;
+        ld_buy = Double.parseDouble(ls_buy) + 0.0 ;
+        ld_cash = Double.parseDouble(ls_cash)+0.0 ;
+        ld_total = ld_buy - ld_cash ;
+        ls_total = ld_total.toString() ;
         //-----
         this.total.setText(ls_total);
     }
