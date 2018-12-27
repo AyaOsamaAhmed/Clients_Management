a50d77f75b96ea9f5381849d815e6355f93f0a9a
AyaOsamaAhmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2018-12-27 17:21:54 +0200

    27-12-2018-part2

--- app/src/main/java/app/Clients_Management/com/ClientsPaid.java
+++ app/src/main/java/app/Clients_Management/com/ClientsPaid.java
@@ -1,11 +1,18 @@
 package app.Clients_Management.com;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
 
 /**
  * Created by egypt2 on 18-Dec-18.
@@ -14,7 +21,11 @@ import android.widget.TextView;
 public class ClientsPaid extends Activity {
 
     Button      button_save;
+    String      ls_id ,ls_username;
 
+    DatabaseReference   databaseclients;
+    DatePickerDialog datePickerDialog ;
+    DataPaid        datapaid ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +36,52 @@ public class ClientsPaid extends Activity {
         button_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                setData();
+                if( validation_data()){
+                    String  id = databaseclients.push().getKey();
+                    ls_id = id ;
+                //    datapaid  = new DataClients(ls_id ,ls_name,ls_phone,ls_card,ls_cash,ls_buy,ls_date);
 
+                    databaseclients.child(id).setValue(datapaid);
+
+                    Toast.makeText(ClientsPaid.this, "Saved Data Sucsses", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ClientsPaid.this,ClientsList.class);
+                    intent.putExtra("username", ls_username);
+                    startActivity(intent);
+                }
             }
+
+
         });
 
     }
 
+    private boolean validation_data() {
 
+        return false ;
+    }
+    private void setData() {
+
+
+    }
+
+    @Override
+public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, android.R.style.Theme_Holo_Light));
+
+        // builder.
+        builder.setMessage("هل انت متاكد من إلغاء انشاء عميل جديد؟").setCancelable(false).setPositiveButton("نعم", new DialogInterface.OnClickListener() {
+public void onClick(DialogInterface dialog, int id) {
+        Intent intent = new Intent(ClientsPaid.this ,ClientsList.class);
+        startActivity(intent);
+
+        }
+        }).setNegativeButton("لا", new DialogInterface.OnClickListener() {
+public void onClick(DialogInterface dialog, int id) {
+        dialog.cancel();
+        }
+        });
+        builder.create().show();
+        }
 
 }
---
+++ app/src/main/java/app/Clients_Management/com/DataPaid.java
@@ -0,0 +1,12 @@
+package app.Clients_Management.com;
+
+/**
+ * Created by egypt2 on 27-Dec-18.
+ */
+
+public class DataPaid  {
+
+
+
+
+}
